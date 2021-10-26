7a0f2a2c504b87229800b61d88462d0678fd1e83
Priyanka-Kasireddy <priyankakasireddy2000@msitprogram.net>
2021-10-26 21:09:01 +0530

    Parse(name,position,state)

--- hw6_social.py
+++ hw6_social.py
@@ -36,7 +36,13 @@ Parameters: str
 Returns: str
 '''
 def parseName(fromString):
-    return
+    for line in fromString.split("\n"):
+        start=line.find(" ")
+        line=line[start:]
+        end=line.find("(")
+        line=line[:end]
+        line=line.strip()
+    return line
 
 
 '''
@@ -46,7 +52,13 @@ Parameters: str
 Returns: str
 '''
 def parsePosition(fromString):
-    return
+    for line in fromString.split("\n"):
+        start=line.find("(")+len("(")
+        line=line[start:]
+        end=line.find(" ")
+        line=line[:end].strip()
+    return line
+    
 
 
 '''
@@ -56,7 +68,12 @@ Parameters: str
 Returns: str
 '''
 def parseState(fromString):
-    return
+    for line in fromString.split("\n"):
+        start=line.find("from")+len("from ")
+        line=line[start:]
+        end=line.find(")")
+        line=line[:end].strip()
+    return line
 
 
 '''
@@ -267,7 +284,11 @@ if __name__ == "__main__":
     # test.week1Tests()
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     # test.runWeek1()
-    test.testMakeDataFrame()
+    # test.testMakeDataFrame()
+    # test.testParseName()
+    # test.testParsePosition()
+    test.testParseState()
+
 
     ## Uncomment these for Week 2 ##
     """print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
