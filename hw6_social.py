42d287a54298f42a3c4cc4f9427989925ea0a25f
Priyanka-Kasireddy <priyankakasireddy2000@msitprogram.net>
2021-10-29 17:09:00 +0530

    Graph Hashtag Sentiments to Frequencies

--- hw6_social.py
+++ hw6_social.py
@@ -312,7 +312,7 @@ def graphTopNStates(stateCounts, stateFeatureCounts, n, title):
     for i in stateFeatureCounts:
         featurerate[i]=(stateFeatureCounts[i]/stateCounts[i])
     topstates=dict(Counter(featurerate).most_common(n))
-    graphStateCounts(topstates,"Top n Featured")
+    graphStateCounts(topstates,title)
     return
 
 
